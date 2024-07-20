6bd01f3ec39821761eea2be92e6195010cf32f89
Alyona <maksalena04@mail.ru>
2024-07-20 20:09:29 +0300

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Алёна Максимова on 20.08.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
